5e96aad8dd030cd2cf0da6fd78cfac24f8e645dc
kaito hasegawa <kaitohasegawa000@gmail.com>
2019-11-10 21:00:30 +0900

    kvファイルボタン追加

--- main_app.py
+++ main_app.py
@@ -1,9 +1,13 @@
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+ 
+class Widgets(Widget):
+    pass
  
 class bottan(App):
     def build(self):
-        return Label()
+        return Widgets()
  
 if __name__ == "__main__":
     bottan().run()

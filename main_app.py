45a8f895638c7c66cf457431327fcee9b04fd3bd
kaito hasegawa <kaitohasegawa000@gmail.com>
2019-11-12 21:54:59 +0900

    ボタンロック追加

--- main_app.py
+++ main_app.py
@@ -3,17 +3,23 @@ from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 import subprocess as sb
 
+global bottan_st = 0
  
 class Widgets(Widget):
     def buttonClicked1(self):
-        global tl
-        tl = sb.Popen(["python", "trade.py"])
-        bottan_st = 1
+        if bottan_st == 0:
+            global tl
+            tl = sb.Popen(["python", "trade.py"])
+            global bottan_st = 1
+        else:
+            pass
 
     def buttonClicked2(self):
-        tl.terminate()
-        # os.kill(pt.pid, signal.SIGINT)
-        bottan_st = 0
+        if bottan_st == 1:
+            tl.terminate()
+            global bottan_st = 0
+        else:
+            pass
  
 class main(App):
     def build(self):

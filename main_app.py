12865009846331bb13a1c5da2abe7631bbe95f93
kaito hasegawa <kaitohasegawa000@gmail.com>
2019-11-12 21:17:48 +0900

    停止ボタン追加

--- main_app.py
+++ main_app.py
@@ -2,13 +2,19 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 import subprocess as sb
+import os
 
  
 class Widgets(Widget):
-    def buttonClicked(self):
-        # import trade
-        pt = sb.Popen(["python", "trade.py"])
-        # print("ok")
+    def buttonClicked1(self):
+        global tl
+        tl = sb.Popen(["python", "trade.py"])
+        bottan_st = 1
+
+    def buttonClicked2(self):
+        tl.terminate()
+        # os.kill(pt.pid, signal.SIGINT)
+        bottan_st = 0
  
 class main(App):
     def build(self):

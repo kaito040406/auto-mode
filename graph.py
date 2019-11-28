49178fc9e9fa717a8c3d7d97e95a3b025e209618
kaito hasegawa <kaitohasegawa000@gmail.com>
2019-11-28 18:33:16 +0900

    出力レンジ変更

--- graph.py
+++ graph.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import mpl_finance as mpf
-from matplotlib import ticker
+import matplotlib.ticker as ticker
 import datetime
 import matplotlib.dates as mdates
-import matplotlib.pyplot as plt
+
 
 xdata = []
 data = pd.read_csv('test.csv', header = None, names=['o','h','i','c'])
@@ -16,7 +16,10 @@ for i in range(len(y)):
   xdata.append(i+1)
 
 x = xdata
-
-
 plt.plot(x, y)
+
+plt.xlim([2000,2350])
+plt.ylim([-300,300])
+plt.yticks( [0, 200, 20] )
+
 plt.show()
